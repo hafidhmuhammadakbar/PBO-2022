85b455748be152d7d87b68947152abc8d4456334
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2022-12-12 10:40:04 +0700

    Add Class Kalkulator

--- src/Kalkulator/src/test/KalkulatorTest.java
+++ src/Kalkulator/src/test/KalkulatorTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.time.Duration;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import main.Kalkulator;
@@ -16,24 +20,32 @@ public class KalkulatorTest {
     @Test
     void tambahTest() {
         assertEquals(5.0, kalkulator.tambah(2.0, 3.0),
-                "penjumlahan sudah jalan");
+                "penjumlahan salah");
     }
 
     @Test
     void kurangTest(){
         assertEquals(6.0, kalkulator.kurang(9.0, 3.0),
-                "pengurangan sudah jalan");
+                "pengurangan salah");
     }
 
     @Test
     void kaliTest(){
         assertEquals(27.0, kalkulator.kali(9.0, 3.0),
-                "kali sudah jalan");
+                "perkalian salah");
     }
 
     @Test
     void bagiTest(){
         assertEquals(3.0, kalkulator.bagi(9.0, 3.0),
-                "pembagian sudah jalan");
+                "pembagian salah");
+    }
+
+    @Test
+    void perkalianBesarTest(){
+        assertTimeout(
+            Duration.ofSeconds(1),
+            ()->kalkulator.kali(99999999.0, 99999999.0)
+        );
     }
 }
